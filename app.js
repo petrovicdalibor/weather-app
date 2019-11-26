3e28d852183aaedd4d41053e12b3302a32aac618
Dalibor Petrovic <daliborpskopje@gmail.com>
2019-11-26 16:26:28 +0100

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ window.addEventListener('load', () => {
             lat = position.coords.latitude;
 
             const proxy = 'http://cors-anywhere.herokuapp.com/';
-            const api = `http://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/5061f908fef335c6fc9765f1ef107ea2/${lat},${long}`;
+            const api = `https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/5061f908fef335c6fc9765f1ef107ea2/${lat},${long}`;
 
             fetch(api)
                 .then(response => {
